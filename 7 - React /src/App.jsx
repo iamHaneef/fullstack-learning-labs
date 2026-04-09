ae73d49be1d73ff9f35e2778d32a9094e6930529
iamHaneef <haneef@gmail.com>
2026-04-09 19:35:47 +0530

    new jsx added

--- 7 - React /src/App.jsx
+++ 7 - React /src/App.jsx
@@ -1,17 +1,12 @@
-import Header from "./Header"
-import Food from "./Food"
-import Footer from "./Footer"
-
+import Students from "./3 - Props/Students"
 function App() {
-  
-  return(
+  return (
     <>
-      <Header />
-      <Food />
-      <Footer />
+      <Students name="Haneef" age={10} no={123} check={true} />
+      <Students name="Aile" age={20} no={456} check={false} />
+      <Students name="Deny" age={30} no={984} check={true}/>
+      <Students name="Dead"  age ={1000}/>
     </>
   )
-
 }
-
 export default App
