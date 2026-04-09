fd68e639a438581b6c2f03a7e4cbb479793f8735
iamHaneef <haneef@gmail.com>
2026-04-09 16:50:58 +0530

    add

---
+++ 7 - React /src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header"
+import Food from "./Food"
+import Footer from "./Footer"
+
+function App() {
+  
+  return(
+    <>
+      <Header />
+      <Food />
+      <Footer />
+    </>
+  )
+
+}
+
+export default App
---
+++ 7 - React /src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <>
+            
+            <h2> &copy; 2023 Haneef Fruit Shop Websiite</h2>
+        </>
+    )
+}
+
+export default Footer;
