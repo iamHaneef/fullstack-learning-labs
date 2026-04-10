0e58d1cadba0afbb73c6705b1ccf8d809f71cdf6
iamHaneef <haneef@gmail.com>
2026-04-10 19:02:38 +0530

    New Concepts

--- 7 - React/src/6 - Click_Event/Click.jsx
+++ 7 - React/src/6 - Click_Event/Click.jsx
@@ -1,6 +1,23 @@
-function Click()
-{
+import styles from "./Style.module.css"
+let count = 0;
+function Click() {
 
+    const handleClick = (e) => {
+        if (count < 3) {
+            count++;
+            e.target.textContent = `You clicked ${count} Times`;
+        }
+        else {
+            e.target.textContent = `Stop Clicking me`;
+        }
+    }
+
+    return (
+        <>
+            <button onlick={(e) => handleClick(e)}
+                className={styles.button}>Submit</button>
+        </>
+    )
 }
 
 export default Click;
